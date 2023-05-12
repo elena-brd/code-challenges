ff48d3b848e6608f22a8a8cfdf3c0e74ed20dce8
elena-brd <128292991+elena-brd@users.noreply.github.com>
2023-05-12 12:20:44 +0200

    Add files via upload

---
+++ Convert a string to an array/main.js
@@ -0,0 +1,8 @@
+function stringToArray(string) {
+    let arr = ' ';
+    return string.split(...arr)
+}
+
+console.log(stringToArray("Robin Singh"));
+console.log(stringToArray("I love arrays they are my favorite"));
+
