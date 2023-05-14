b91bf5deb67d608f7c15e2d8dbe4fb72adf9fae2
elena-brd <128292991+elena-brd@users.noreply.github.com>
2023-05-14 11:16:39 +0200

    Add files via upload

---
+++ Sum The Strings/main.js
@@ -0,0 +1,13 @@
+function sumStr(a, b) {
+    let x = Number(a) + Number(b);
+    return String(x)
+}
+
+console.log(sumStr("5", "5"));
+console.log(sumStr("4", "5"));
+console.log(sumStr("34", "5"));
+console.log(sumStr("", ""));
+console.log(sumStr("2", ""));
+console.log(sumStr("-5", "3"));
+
+
