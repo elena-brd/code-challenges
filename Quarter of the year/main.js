6777c0ea3a88f8e1d195685fbdbf1099000669a6
elena-brd <128292991+elena-brd@users.noreply.github.com>
2023-05-17 17:07:23 +0200

    Add files via upload

---
+++ Quarter of the year/main.js
@@ -0,0 +1,18 @@
+const quarterOf = (month) => {
+    if (month <= 3) {
+        return 1;
+    } else if (month <= 6) {
+        return 2;
+    } else if (month <= 9) {
+        return 3;
+    } else if (month <= 12) {
+        return 4
+    } else {
+        return 'Not Valid'
+    }
+}
+
+console.log(quarterOf(3));
+console.log(quarterOf(8));
+console.log(quarterOf(11));
+console.log(quarterOf(14));
